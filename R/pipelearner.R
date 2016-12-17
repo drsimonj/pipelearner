945824388dcaefc04c4deff6c92cc76074f85306
drsimonj <drsimonjackson@gmail.com>
2016-12-17 14:24:14 +1100

    Init pipelearner()

---
+++ R/pipelearner.R
@@ -0,0 +1,25 @@
+#' Create a new pipe learner
+#'
+#' \code{pipelearner} initializes a new pipelearner object. It is used to
+#' specify the input data frame to subsequent machine learning algorithms.
+#'
+#' @param data Data frame containing all variables needed by learning algorithm
+#'   formulae.
+#' @export
+pipelearner <- function(data = NULL) {
+  UseMethod("pipelearner")
+}
+
+#' @export
+pipelearner.default <- function(data = NULL) {
+  pipelearner.data.frame(as.data.frame(data))
+}
+
+#' @export
+pipelearner.data.frame <- function(data) {
+  pl <- structure(list(
+    data = data
+  ), class = c("pipelearner"))
+
+  pl
+}
