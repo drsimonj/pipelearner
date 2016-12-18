3f0d43fbea6b5724f3500bf01f3696c0961522f6
drsimonj <drsimonjackson@gmail.com>
2016-12-19 08:53:27 +1100

    Add predicted results to learn()

--- R/learn.R
+++ R/learn.R
@@ -26,15 +26,20 @@ learn.pipelearner <- function(pl) {
 }
 
 
-fit_cvdata <- function(train_data, cv_id, models) {
+fit_cvdata <- function(train_data, test_data, cv_id, models) {
   models %>%
     dplyr::mutate(fit = purrr::invoke_map(.$.f, .$params, data = train_data),
+                  predicted_train = purrr::map(fit, predict, newdata = train_data),
+                  predicted_test  = purrr::map(fit, predict, newdata = test_data),
                  #outcome_var = purrr::map_chr(.$params, ~ as.character(lazyeval::f_lhs(.$formula))),
                   cv.id = cv_id)
 }
 
 fit_p <- function(p, cv_tbl, models) {
   data_list <- purrr::map(cv_tbl$train, ~ as.data.frame(.) %>% dplyr::sample_frac(p))
-  purrr::map2_df(data_list, cv_tbl$.id, fit_cvdata, models = models) %>%
+  #purrr::map2_df(data_list, cv_tbl$.id, fit_cvdata, models = models) %>%
+  to_fit <- list(train_data = data_list, test_data = cv_tbl$test, cv_id = cv_tbl$.id)
+  purrr::pmap_df(to_fit, fit_cvdata, models = models) %>%
     dplyr::mutate(train.p = p)
+
 }
