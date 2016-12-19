07fd34a52c41d0eedd5dc60bb81cb41f08261846
drsimonj <drsimonjackson@gmail.com>
2016-12-19 21:25:03 +1100

    snake case $fits names

--- R/learn.R
+++ R/learn.R
@@ -32,7 +32,7 @@ fit_cvdata <- function(train_data, test_data, cv_id, models) {
                   predicted_train = purrr::map(fit, predict, newdata = train_data),
                   predicted_test  = purrr::map(fit, predict, newdata = test_data),
                  #outcome_var = purrr::map_chr(.$params, ~ as.character(lazyeval::f_lhs(.$formula))),
-                  cv.id = cv_id)
+                  cv_pair = cv_id)
 }
 
 fit_p <- function(p, cv_tbl, models) {
@@ -40,6 +40,6 @@ fit_p <- function(p, cv_tbl, models) {
   #purrr::map2_df(data_list, cv_tbl$.id, fit_cvdata, models = models) %>%
   to_fit <- list(train_data = data_list, test_data = cv_tbl$test, cv_id = cv_tbl$.id)
   purrr::pmap_df(to_fit, fit_cvdata, models = models) %>%
-    dplyr::mutate(train.p = p)
+    dplyr::mutate(train_p = p)
 
 }
